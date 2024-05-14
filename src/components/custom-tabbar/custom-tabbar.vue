ac1dc45020da8b8faaf4c0412c494e19a5730a2d
gyl <gyl@acewill.cn>
2024-05-14 23:28:33 +0800

    首页腐蚀信息

--- src/components/custom-tabbar/custom-tabbar.vue
+++ src/components/custom-tabbar/custom-tabbar.vue
@@ -1,7 +1,13 @@
 <template>
-  <nut-tabbar v-model:visible="currnetIndex" :bottom="true" :safe-area-inset-bottom="true" active-color="#1E90FF" @tab-switch="tabSwitch">
+  <nut-tabbar 
+    v-model:visible="currnetIndex"
+    :bottom="true"
+    :safe-area-inset-bottom="true"
+    active-color="#1E90FF"
+    @tab-switch="tabSwitch">
     <nut-tabbar-item tab-title="首页" to="/pages/index/index" icon="home"></nut-tabbar-item>
-    <nut-tabbar-item tab-title="设备" to="/pages/product/index" icon="category"></nut-tabbar-item>
+    <nut-tabbar-item tab-title="监听设备" to="/pages/product/index" icon="voice"></nut-tabbar-item>
+    <nut-tabbar-item tab-title="加注设备" to="/pages/product/index" icon="category"></nut-tabbar-item>
     <nut-tabbar-item tab-title="我的" to="/pages/mine/index" icon="my"></nut-tabbar-item>
   </nut-tabbar>
 </template>
@@ -16,12 +22,10 @@ const tablist = [
 ]
 let currnetIndex = ref(0)
 const currentPage =  Taro.getCurrentInstance().router?.path || ''
-console.log(currentPage, 'currentPage')
 onMounted(() => {
   currnetIndex.value = tablist.indexOf(currentPage)
 })
 const tabSwitch = (item, index) => {
-  console.log(item, index, 'val')
   Taro.redirectTo({
     url: tablist[index]
   });
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -33,17 +33,22 @@
       </view>
     </view>
     <view class="app-main">
-      <view class="equipment-list">
+      <!-- <view class="equipment-list">
         <view class="equipment-title">关注设备</view>
         <view class="equipment-item" v-for="item in 5" :key="item" @tap="toDetail">
-          <!-- 常用设备 -->
           <view class="equipment-info">
             <text class="name">设备{{ item }}</text>
             <text class="desc">压力：22，温度：50度,腐蚀效率：30%</text>
           </view>
           <nut-circleprogress :progress="item * 10" color="#1E90FF"> </nut-circleprogress>
         </view>
-      </view>
+      </view> -->
+      
+    </view>
+    
+    <view class="line-canvas">
+      <view class="equipment-title">腐蚀信息</view>
+      <EChart ref="canvas" />
     </view>
     <CustomTabBar />
   </view>
@@ -55,6 +60,10 @@ import { useShareAppMessage, usePageScroll } from '@tarojs/taro'
 import CustomNavBar from '../../components/custom-nav-bar/custom-nav-bar'
 import CustomTabBar from '../../components/custom-tabbar/custom-tabbar'
 import "./index.scss";
+import * as echarts from "echarts4taro3/lib/assets/echarts"; // 这里用了内置的，也可以用自定义的 echarts.js
+import { EChart, loadEcharts } from "echarts4taro3";
+loadEcharts(echarts);
+const canvas = ref(null);
 let showBg = ref(false)
 let showInit = ref(false)
 let nowLocation = ref({})
@@ -71,18 +80,6 @@ const toDetail = () => {
   })
 }
 const markers = ref([])
-// const polyline = [{
-//   points: [{
-//     longitude: 113.3245211,
-//     latitude: 23.10229
-//   }, {
-//     longitude: 113.324520,
-//     latitude: 23.21229
-//   }],
-//   color:"#FF0000DD",
-//   width: 2,
-//   dottedLine: true
-// }]
 const regionchange = (e) =>  {
   console.log(e.type)
 }
@@ -104,16 +101,161 @@ const initLocation = () => {
         width: 50,
         height: 50
       })
-      // const latitude = res.latitude
-      // const longitude = res.longitude
-      // const speed = res.speed
-      // const accuracy = res.accuracy
     }
   })
 }
 initLocation()
 onMounted(() => {
-  Taro.nextTick(() => {});
+  const echartComponentInstance = canvas.value;
+  let base = +new Date(2024, 4, 5);
+  let oneDay = 24 * 3600 * 1000;
+  let valueBase = Math.random() * 300;
+  let valueBase2 = Math.random() * 50;
+  let data = [];
+  let data2 = [];
+  for (var i = 1; i < 10; i++) {
+    var now = new Date((base += oneDay));
+    var dayStr = [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('-');
+    valueBase = Math.round((Math.random() - 0.5) * 20 + valueBase);
+    valueBase <= 0 && (valueBase = Math.random() * 300);
+    data.push([dayStr, valueBase]);
+    valueBase2 = Math.round((Math.random() - 0.5) * 20 + valueBase2);
+    valueBase2 <= 0 && (valueBase2 = Math.random() * 50);
+    data2.push([dayStr, valueBase2]);
+  }
+  let option = {
+    // title: {
+    //   left: 'center',
+    //   text: 'Tootip and dataZoom on Mobile Device'
+    // },
+    legend: {
+      top: 'bottom',
+      data: ['Intention']
+    },
+    tooltip: {
+      triggerOn: 'none',
+      position: function (pt) {
+        return [pt[0], 130];
+      }
+    },
+    // toolbox: {
+    //   left: 'center',
+    //   itemSize: 25,
+    //   top: 55,
+    //   feature: {
+    //     dataZoom: {
+    //       yAxisIndex: 'none'
+    //     },
+    //     restore: {}
+    //   }
+    // },
+    xAxis: {
+      type: 'time',
+      axisPointer: {
+        value: '2025-5-14',
+        snap: true,
+        lineStyle: {
+          color: '#7581BD',
+          width: 2
+        },
+        label: {
+          show: true,
+          formatter: function (params) {
+            return echarts.format.formatTime('yyyy-MM-dd', params.value);
+          },
+          backgroundColor: '#7581BD'
+        },
+        handle: {
+          show: true,
+          color: '#7581BD'
+        }
+      },
+      splitLine: {
+        show: false
+      }
+    },
+    yAxis: {
+      type: 'value',
+      axisTick: {
+        inside: true
+      },
+      splitLine: {
+        show: false
+      },
+      axisLabel: {
+        inside: true,
+        formatter: '{value}\n'
+      },
+      z: 10
+    },
+    grid: {
+      top: 110,
+      left: 15,
+      right: 15,
+      height: 160
+    },
+    dataZoom: [
+      {
+        type: 'inside',
+        throttle: 50
+      }
+    ],
+    series: [
+      {
+        name: '厚度',
+        type: 'line',
+        smooth: true,
+        symbol: 'circle',
+        symbolSize: 5,
+        sampling: 'average',
+        itemStyle: {
+          color: '#0770FF'
+        },
+        stack: 'a',
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: 'rgba(58,77,233,0.8)'
+            },
+            {
+              offset: 1,
+              color: 'rgba(58,77,233,0.3)'
+            }
+          ])
+        },
+        data: data
+      },
+      {
+        name: '腐蚀度',
+        type: 'line',
+        smooth: true,
+        stack: 'a',
+        symbol: 'circle',
+        symbolSize: 5,
+        sampling: 'average',
+        itemStyle: {
+          color: '#F2597F'
+        },
+        areaStyle: {
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: 'rgba(213,72,120,0.8)'
+            },
+            {
+              offset: 1,
+              color: 'rgba(213,72,120,0.3)'
+            }
+          ])
+        },
+        data: data2
+      }
+    ]
+  };
+  Taro.nextTick(() => {
+    echartComponentInstance.refresh(option);
+  });
 });
 usePageScroll((payload) => {
   const { scrollTop } = payload
