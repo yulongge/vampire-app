d255af6ad4be6b3fdf91c4e14971c6c95cf198d1
gyl <gyl@acewill.cn>
2024-06-29 22:59:44 +0800

    update: 监测设备类别支持搜索

--- src/package/monitor/pages/index/index.vue
+++ src/package/monitor/pages/index/index.vue
@@ -1,8 +1,16 @@
 <template>
   <view class="product-wrap">
-    <view class="filter">
+    <!-- <view class="filter">
       <nut-button type="info" @tap="toAnalysisData">数据分析</nut-button>
       <nut-button type="info" @tap="addMonitor">添加监测设备</nut-button>
+    </view> -->
+    <view class="filter">
+      <nut-searchbar v-model="keyword">
+        <template v-slot:rightout>
+          <span @click="toSearchMonitor">搜索</span>
+          <!-- <span class="add-con" @click="toAddUser"><nut-icon name="plus" ></nut-icon></span> -->
+        </template>
+      </nut-searchbar>
     </view>
     <view class="equipment-list">
       <view class="equipment-item" v-for="item in 20" :key="item" @tap="toDetail">
@@ -18,16 +26,32 @@
       </view>
     </view>
     <CustomTabBar />
+    <nut-fixednav :position="{bottom:'280px' }" type="right" v-model:visible="showNav">
+      <template v-slot:list>
+        <ul class="nut-fixednav__list">
+            <li class="nut-fixednav__list-item" @tap="toAnalysisData">数据分析</li>
+            <li class="nut-fixednav__list-item" @tap="addMonitor">添加设备</li>
+            <!-- <li class="nut-fixednav__list-item" @tap="toStatistics">统计报表</li> -->
+        </ul>
+      </template>
+      <template v-slot:btn>
+          <nut-icon name="retweet" color="#fff">
+          </nut-icon>
+          <span class="text">{{ showNav ? '操作设备' : '操作设备' }}</span>
+      </template>
+    </nut-fixednav>
   </view>
 </template>
 <script setup>
 import { ref, onMounted, reactive } from "vue";
 import Taro from "@tarojs/taro";
-import { useShareAppMessage } from '@tarojs/taro';
+import { useShareAppMessage, useDidShow } from '@tarojs/taro';
 import CustomTabBar from '@/components/custom-tabbar/custom-tabbar'
 import "./index.scss";
 import { redirect } from '@/utils/redirect';
 import { getStorageSync } from '@/utils/storage'
+let keyword = ref('')
+let showNav = ref(false)
 const toDetail = () => {
   Taro.navigateTo({
     url: '/package/monitor/pages/monitorDetail/index'
@@ -48,6 +72,12 @@ const toAnalysisData = () => {
     url: '/package/monitor/pages/analysicAll/index'
   })
 }
+const toSearchMonitor = () => {
+
+}
+useDidShow(() => {
+  showNav.value = false
+})
 onMounted(() => {
   const userInfo = getStorageSync('userInfo')
   if (!userInfo?.id) {
